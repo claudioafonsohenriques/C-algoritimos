103c941cac4137dee3c97101c4661a9525cae75d
Claúdio Henriques <124451351+claudioafonsohenriques@users.noreply.github.com>
2023-06-17 15:45:15 +0100

    Atualizar o Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main(){
+
+   int n;
+   pronto("Digite o numero para ver a tabuada:\n");
+   scanf ("%d",&n);
+   for(int y=0; y<=12; y++){
+   printf("%d X %d = %d\n",n,y,n*y);
+   }
+//Benilde está tudo certo?
+}
